c7134ee6c084cf568296bdd235d70d17d2fd977d
NathanNguyen <khanhngoc0102@gmail.com>
2023-01-24 19:33:21 -0800

    Updates

---
+++ precache-manifest.266d61c2068ba96c5c609e45aa37c853.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82cf8bfbaed1e980f254a5ff82681f0",
+    "url": "/attemp1/index.html"
+  },
+  {
+    "revision": "f9a83d269795454b9bfa",
+    "url": "/attemp1/static/css/main.d5d31f16.chunk.css"
+  },
+  {
+    "revision": "75267617a7cd558b0c24",
+    "url": "/attemp1/static/js/2.af0d4b87.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/attemp1/static/js/2.af0d4b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91867c66df1ec33e2514",
+    "url": "/attemp1/static/js/3.8bd1639f.chunk.js"
+  },
+  {
+    "revision": "f9a83d269795454b9bfa",
+    "url": "/attemp1/static/js/main.a4fbe530.chunk.js"
+  },
+  {
+    "revision": "cea4048103f224bd7166",
+    "url": "/attemp1/static/js/runtime-main.c6dbe130.js"
+  },
+  {
+    "revision": "b21c84b6f382661cf99794bd50c275e3",
+    "url": "/attemp1/static/media/1.b21c84b6.jfif"
+  },
+  {
+    "revision": "e4a20cf406ff0a31378d7ab33ba8c1ba",
+    "url": "/attemp1/static/media/Owais' Resume.e4a20cf4.pdf"
+  },
+  {
+    "revision": "5035699ad38fd5313bc95c4891f53581",
+    "url": "/attemp1/static/media/auto-tracker-image.5035699a.png"
+  },
+  {
+    "revision": "09e6ae3da5daad49c278e9ff4a75743d",
+    "url": "/attemp1/static/media/budget-tracker-image.09e6ae3d.png"
+  },
+  {
+    "revision": "d92efe2500ba8695d4ae6432f75b5cf9",
+    "url": "/attemp1/static/media/flash-beats-image.d92efe25.gif"
+  },
+  {
+    "revision": "d8671f834c3c32005de3dcb406167c84",
+    "url": "/attemp1/static/media/living-real-image.d8671f83.png"
+  },
+  {
+    "revision": "fd14fff8beb9ae1bb3fcdf093419fe49",
+    "url": "/attemp1/static/media/note-taker-image.fd14fff8.png"
+  },
+  {
+    "revision": "0e54790e036d3e97597027712ffaab9b",
+    "url": "/attemp1/static/media/password-generator-image.0e54790e.png"
+  },
+  {
+    "revision": "e232e25f978fc2e6a3f3960c7dbcb124",
+    "url": "/attemp1/static/media/recent-photo.e232e25f.jpg"
+  },
+  {
+    "revision": "5db07b5ca4b360c2f9058145e3070ff4",
+    "url": "/attemp1/static/media/weather-dashboard-image.5db07b5c.png"
+  },
+  {
+    "revision": "2524a8b5b4006da1d062430b9f8b0c05",
+    "url": "/attemp1/static/media/webpage-screenshot.2524a8b5.png"
+  }
+]);
